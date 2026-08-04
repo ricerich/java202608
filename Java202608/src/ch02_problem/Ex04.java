1a7b709a3359ea6f26b3e8b19772b256670730c1
lsy3709 <lsy3709@naver.com>
2026-08-04 16:14:05 +0900

    ex5

--- Java202608/src/ch02_problem/Ex04.java
+++ Java202608/src/ch02_problem/Ex04.java
@@ -1,10 +1,40 @@
 package ch02_problem;
 
+import java.util.Scanner;
+
 public class Ex04 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner sc1 = new Scanner(System.in);
+		
+		System.out.print("정수 3개 입력>>");
+		int n1 = sc1.nextInt();
+		int n2 = sc1.nextInt();
+		int n3 = sc1.nextInt();
+		
+		//중간값을 저장할 변수
+		int median=0;
+		
+		if( (n1>=n2 && n1<=n3) || (n1>=n3 && n1<=n2)) { //n1이 중간일 경우
+			median = n1;
+		}
+		else if((n2>=n1 && n2<=n3) || (n2>=n3 && n2<=n1)) { //n2이 중간일 경우
+			median = n2;
+		}
+		else if((n3>=n1 && n3<=n2) || (n3>=n2 && n3<=n1)) { //n3이 중간일 경우 
+			median = n3;
+		}
 
+		System.out.println("중간 값은 "+ median +"");
+		sc1.close();
 	}
-
 }
+
+
+
+
+
+
+
+
--- Java202608/src/ch02_problem/Ex05.java
+++ Java202608/src/ch02_problem/Ex05.java
@@ -1,10 +1,41 @@
 package ch02_problem;
 
+import java.util.Scanner;
+
 public class Ex05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc1 = new Scanner(System.in);
+		
+		System.out.print("정수 3개 입력>>");
+		int a = sc1.nextInt();
+		int b = sc1.nextInt();
+		int c = sc1.nextInt();
+		
+//		//삼각형이 안되는 경우
+//		if( (a+b)<c || (b+c)<a || (a+c)<b ) {
+//			System.out.println("삼각형이 될수 없어요!");
+//		}
+//		else
+//			System.out.println("삼각형이 되요!");
+		
+		//삼각형이 되는 경우
+		if( (a+b)>c && (b+c)>a && (a+c)>b ) {
+			System.out.println("삼각형이 되요!");
+		}
+		else
+			System.out.println("삼각형이 될수 없어요!");
+		
+		sc1.close();
+		
 	}
 
 }
+
+
+
+
+
+
+
+
