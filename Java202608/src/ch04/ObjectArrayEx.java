fdefddd794f02cb2c3a3abddcd6f773be3f0a786
ricerich <ricerich@gmail.com>
2026-08-12 15:13:00 +0900

    1

---
+++ Java202608/src/ch04/ObjectArrayEx.java
@@ -0,0 +1,33 @@
+package ch04;
+
+class Circle1{
+	int radius; // 원의 반지름 필드
+	String name; // 원의 이름을 필드
+
+	public double getArea() { // 멤버 메소드
+		return 3.14 * radius * radius;
+	}
+}
+
+
+public class ObjectArrayEx {
+
+	public static void main(String[] args) 
+	{
+		//객체 배열 만들고, 사용하기
+		
+		int arr2 [] = new int[3];
+		Circle1 arr1[] = new Circle1[3];
+		
+
+	}
+
+}
+
+
+
+
+
+
+
+
