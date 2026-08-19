47dcd2e064a55dedc31f86c772534ad27e6c6f44
ricerich <ricerich@gmail.com>
2026-08-19 14:24:41 +0900

    99999

---
+++ Java202608/src/ch04_problem/Ex09.java
@@ -0,0 +1,39 @@
+package ch04_problem;
+
+class ArrayUtil 
+{
+	public static int[] concat(int[] a, int[] b) 
+	{
+		int c[] = new int[a.length + b.length];
+		
+		for(int i=0; i<a.length; i++) {
+			c[i] = a[i];
+		}
+		
+		for(int i=0; i<b.length; i++) {
+			c[a.length+i] = b[i];
+		}
+		
+		return c;		
+	}
+
+	public static void print(int[] a) 
+	{
+		System.out.print("[ ");
+		for(int i=0; i<a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.print(" ]");
+	}
+}
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		int[] array1 = { 1, 5, 7, 9 };
+		int[] array2 = { 3, 6, -1, 100, 77 };
+		int[] array3 = ArrayUtil.concat(array1, array2);
+		ArrayUtil.print(array3);
+	}
+
+}
