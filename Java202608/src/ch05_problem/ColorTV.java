cd91556692b65cb5d3e7bfecb3dec9612eaa9de1
ricerich <ricerich@gmail.com>
2026-08-20 17:47:20 +0900

    122

---
+++ Java202608/src/ch05_problem/ColorTV.java
@@ -0,0 +1,20 @@
+package ch05_problem;
+
+public class ColorTV extends TV
+{
+	private int color;
+
+	public ColorTV(int size, int color) 
+	{
+		super(size);
+		this.color = color;
+	}
+
+	public void printProperty() {
+		System.out.println(getSize()+"인치 "+color+"컬러");
+	}
+
+}
+
+
+
---
+++ Java202608/src/ch05_problem/IPTV.java
@@ -0,0 +1,19 @@
+package ch05_problem;
+
+public class IPTV extends ColorTV {
+
+	private String ip;
+	
+	public IPTV(String ip, int size, int color) 
+	{
+		super(size, color);
+		this.ip = ip;
+	}
+	
+	public void printProperty() {
+		System.out.print("나의 IPTV는 "+ip+" 주소의 ");
+		super.printProperty();
+		
+	}
+
+}
